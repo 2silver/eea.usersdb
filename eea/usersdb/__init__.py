c440cd90d6e3c2630db53a1a497c8d9ae6f13a03
moregale <moregale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-03-07 13:47:15 +0000

    nice api imports; bump version to 1.0
    git-svn-id: https://svn.eionet.europa.eu/repositories/Zope/trunk/eea.usersdb@20269 24821446-e7ea-0310-81ac-b189f4507b0b

---
+++ eea/usersdb/__init__.py
@@ -0,0 +1,4 @@
+from db_agent import UsersDB
+from db_agent import editable_user_fields, editable_org_fields
+from db_agent import OrgRenameError, NameAlreadyExists, RoleNotFound
+from db_agent import ORG_LITERAL, ORG_BY_ID
