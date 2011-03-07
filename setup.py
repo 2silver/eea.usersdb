e1f067ca40674f0a3c753103f6b4eb5068c78c5f
moregale <moregale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-03-07 13:19:17 +0000

    import skeleton package for eea.usersdb
    git-svn-id: https://svn.eionet.europa.eu/repositories/Zope/trunk/eea.usersdb@20259 24821446-e7ea-0310-81ac-b189f4507b0b

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='eea.usersdb',
+    version='0.1',
+    author='Eau de Web',
+    author_email='office@eaudeweb.ro',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['python-ldap'],
+)
